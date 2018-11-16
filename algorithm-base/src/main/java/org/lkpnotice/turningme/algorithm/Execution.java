2914d7721b410b5ee296c7d657718d7227c36f81
liujinpeng04 <liujinpeng04@meituan.com>
2018-11-16 13:46:20 +0800

    bi-search for sorted-repeated-array to find count of key

---
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/Execution.java
@@ -0,0 +1,8 @@
+package org.lkpnotice.turningme.algorithm;
+
+/**
+ * Created by liujinpeng on 2018/11/16.
+ */
+public interface Execution {
+    public void execute();
+}
--- algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/N37SortArraySearchCount.java
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/N37SortArraySearchCount.java
@@ -1,8 +1,134 @@
 package org.lkpnotice.turningme.algorithm.joffer;
 
+import org.lkpnotice.turningme.algorithm.Execution;
+
+import java.util.Comparator;
+
 /**
  * Created by liujinpeng on 2018/11/15.
  */
-public class N37SortArraySearchCount {
+public class N37SortArraySearchCount implements Execution{
 
+
+
+    protected Integer[] mockArray(){
+        return new Integer[]{1,2,3};
+    }
+
+    protected Integer mockKey(){
+        return Integer.valueOf(2);
+    }
+
+    public void execute() {
+        Comparable[] sortedArray = mockArray();
+        Comparable key = mockKey();
+        int cnt =statOccuranceInSortedArray(sortedArray,key);
+        System.out.println("result is " + cnt);
+    }
+
+
+    protected Integer statOccuranceInSortedArray(Comparable[] sortedArray,Comparable key){
+        Integer count = 0;
+        if (null == sortedArray || 0 == sortedArray.length ){
+            return count;
+        }
+
+        //input is legal
+
+        //use bi-part alg to explore the position the key on, - return if not found
+        //then deduce pre and after to check the total count
+        int postion = positionKeyInArray(sortedArray,key);
+        if ( -1 != postion ){
+            count = countNumber(sortedArray,postion);
+        }
+
+
+        return count ;
+    }
+
+
+    /**
+     *
+     *
+     * @param sortedArray
+     * @param key
+     * @return -1 if not found . else the first found position .
+     */
+    protected int positionKeyInArray(Comparable[] sortedArray, Comparable key){
+        int result = -1;
+
+        int start = 0;
+        int end = sortedArray.length -1 ;
+
+        while(end >= start){
+
+
+
+
+            int mid = (end +start) /2;
+            //handle the situation that first == tail.., need no iteration more...
+            if (end - start <=1){
+                if (key.compareTo(sortedArray[start]) == 0){
+                    result = start;
+                }else if (key.compareTo(sortedArray[end]) == 0){
+                    result = end;
+                }
+                break;
+            }
+
+
+            //less ,then the end change to the mid
+            if (key.compareTo(sortedArray[mid]) < 0){
+                end = mid;
+            }else if (key.compareTo(sortedArray[mid]) >0){
+                start = mid;
+            }else{
+                //equal then return the position..
+                result = mid;
+                break;
+            }
+
+        }
+
+
+        return result;
+    }
+
+
+    /**
+     *
+     * @param sortedArray
+     * @param keyPos
+     * @return
+     */
+    protected  int countNumber(Comparable[] sortedArray, int keyPos){
+        int preCnt = 0;
+        int alterCnt = 0;
+        for (int i = keyPos-1; i>=0;i--){
+            if (sortedArray[keyPos].compareTo(sortedArray[i]) ==0){
+                preCnt ++;
+            }else{
+                break;
+            }
+        }
+
+        for (int j = keyPos +1 ;j<sortedArray.length ;j++){
+            if (sortedArray[keyPos].compareTo(sortedArray[j]) ==0){
+                alterCnt ++;
+            }else{
+                break;
+            }
+        }
+
+        return preCnt + alterCnt +1;
+    }
+
+
+
+
+
+
+    public static void main(String[] args){
+        new N37SortArraySearchCount().execute();
+    }
 }
