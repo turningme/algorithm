1f7d7f23eebf6ed0fea13a24b49ba34062f53c79
liujinpeng04 <liujinpeng04@meituan.com>
2018-11-29 14:08:51 +0800

    init common module structure

---
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/N41Sum100Sequence.java
@@ -0,0 +1,9 @@
+package org.lkpnotice.turningme.algorithm.joffer;
+
+/**
+ * Created by liujinpeng on 2018/11/16.
+ * Problem: given a number 100, what kinds of sequence number could be summed equal to it ?
+ */
+public class N41Sum100Sequence {
+
+}
---
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/No51ArrayA2ArrayBConstruct.java
@@ -0,0 +1,11 @@
+package org.lkpnotice.turningme.algorithm.joffer;
+
+/**
+ * Created by liujinpeng on 2018/11/29.
+ * A[0,1,...,n-1] n elements
+ * B[0,1,...,n1] n elements
+ * B[i] = A[0]* ...*A[i-1]*A[i+1]*...*A[n]
+ * div operate is not allowed
+ */
+public class No51ArrayA2ArrayBConstruct {
+}
