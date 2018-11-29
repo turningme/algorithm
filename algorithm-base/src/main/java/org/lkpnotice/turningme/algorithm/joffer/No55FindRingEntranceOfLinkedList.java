72d944ca240b2d08a97ef43403cec77e16497e30
liujinpeng04 <liujinpeng04@meituan.com>
2018-11-29 17:59:50 +0800

    complete one solution of joffer no 55

--- algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/No55FindRingEntranceOfLinkedList.java
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/joffer/No55FindRingEntranceOfLinkedList.java
@@ -1,10 +1,13 @@
 package org.lkpnotice.turningme.algorithm.joffer;
 
 import org.lkpnotice.turningme.algorithm.Execution;
+import org.lkpnotice.turningme.algorithm.util.LogUtil;
 import org.lkpnotice.turningme.algorithm.util.Node;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by liujinpeng on 2018/11/29.
@@ -12,20 +15,89 @@ import java.util.List;
  * problem:
  * given a linked list A, find the entrance of ring if exists,otherwise return null
  *
+ * another problem like this is given List A and List B find the shared node if exists, otherwise return null
+ *
+ * solution_a:
+ *  the detect point is that last node of the linked list is null if no ring exists,otherwise it never ends
+ *  so use a set to cache nodes traversed , then compare current node with the set to detect existence , until next is null
+ *  if a ring exists , we will always find a node that we have cached
+ *  but this method need all
+ *
  *
  */
 public class No55FindRingEntranceOfLinkedList implements Execution{
 
-    public void execute() {
 
+    public void execute() {
+        Node nodeList = constructRingLinkedList();
+        Node result = findEntranceOfRing(nodeList);
+        if (null == result){
+            LogUtil.doLog("No ring exists");
+        }else {
+            LogUtil.doLog("ring exists");
+        }
     }
+
+
+    protected  Node constructNoRingLinkedList(){
+        Node n1 = new Node(null);
+        Node n2 = new Node(null);
+
+        Node n3 = new Node(null);
+        Node n4 = new Node(null);
+
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+
+        return n1;
+    }
+
+
+    protected  Node constructRingLinkedList(){
+        Node n1 = new Node(null);
+        Node n2 = new Node(null);
+
+        Node n3 = new Node(null);
+        Node n4 = new Node(null);
+
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+
+        n4.setNext(n2);
+        return n1;
+    }
+
 
 
     /**
      *
      * @param nodeList
      */
-    public void findEntranceOfRing(Node<Integer> nodeList){
+    public Node findEntranceOfRing(Node<Integer> nodeList){
+        Set<Node> cache = new HashSet<Node>();
 
+        Node n = nodeList;
+        Node target = null;
+
+        while(n != null){
+            if (cache.contains(n)){
+                target = n;
+                break;
+            }
+
+            cache.add(n);
+            n = n.getNext();
+        }
+
+        return target;
     }
+
+
+    public static void main(String[] args){
+        new No55FindRingEntranceOfLinkedList().execute();
+    }
+
+
 }
---
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/algorithm/util/LogUtil.java
@@ -0,0 +1,10 @@
+package org.lkpnotice.turningme.algorithm.util;
+
+/**
+ * Created by liujinpeng on 2018/11/29.
+ */
+public class LogUtil {
+    public static void doLog(String in){
+        System.out.println(in);
+    }
+}
