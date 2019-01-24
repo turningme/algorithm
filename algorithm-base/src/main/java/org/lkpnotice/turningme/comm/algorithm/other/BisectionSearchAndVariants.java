ed4be0193283ad9c7b6839a3c8e56bb0e8dadcd6
liujinpeng04 <liujinpeng04@meituan.com>
2019-01-24 15:38:22 +0800

    init for bisection alg and its variation finished

--- algorithm-base/src/main/java/org/lkpnotice/turningme/comm/algorithm/other/BisectionSearchAndVariants.java
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/comm/algorithm/other/BisectionSearchAndVariants.java
@@ -19,6 +19,9 @@ package org.lkpnotice.turningme.comm.algorithm.other;
  *
  * }
  *
+ * the last status ,if not found or found in some cases, start = end + 1
+ * first start = end then next round start = end + 1
+ *
  */
 public class BisectionSearchAndVariants {
 
@@ -27,7 +30,7 @@ public class BisectionSearchAndVariants {
      * @return
      */
     static int[] getInputData(){
-        return new int[]{1,2,3,8,11,16,19,22,23};
+        return new int[]{1,2,3,3,4,8,11,16,19,22,23};
     }
 
     static int getTargetValue(){
@@ -75,10 +78,218 @@ public class BisectionSearchAndVariants {
     }
 
 
+    /**
+     * note that input's elements may be repeated ,and find the first position k where array[k] = v
+     * @param input
+     * @param v
+     */
+    void solutionFirstPositionEqual(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
 
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                end = mid -1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (end + 1 <input.length && input[end+1] == v){
+            position = end + 1;
+        }
+
+        System.out.println(String.format("the first position is array equal %s is %s",v,position));
+
+    }
+
+
+    /**
+     * note that input's elements may be repeated ,and find the last position k where array[k] = v
+     * @param input
+     * @param v
+     */
+    void solutionLastPositionEqual(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
+
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                start = mid + 1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (end >=0 && input[end] == v){
+            position = end;
+        }
+
+        System.out.println(String.format("the last position is array equal %s is %s",v,position));
+    }
+
+
+    /**
+     * note that input's elements may be repeated ,and find the first position k where array[k] >= v
+     * @param input
+     * @param v
+     */
+    void solutionFistPositionEqualOrMore(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
+
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                end = mid - 1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (start <input.length){
+            position = start;
+        }
+
+        System.out.println(String.format("the last position is array equal %s is %s",v,position));
+    }
+
+
+
+    /**
+     * note that input's elements may be repeated ,and find the first position k where array[k] > v
+     * @param input
+     * @param v
+     */
+    void solutionFistPositionMore(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
+
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                start = mid + 1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (start <input.length){
+            position = start;
+        }
+
+        System.out.println(String.format("the last position is array equal %s is %s",v,position));
+    }
+
+
+    /**
+     * note that input's elements may be repeated ,and find the last position k where array[k] <= v
+     * @param input
+     * @param v
+     */
+    void solutionLastPositionEqualOrLess(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
+
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                start = mid + 1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (end >=0){
+            position = end;
+        }
+
+        System.out.println(String.format("the last position is array <= %s is %s",v,position));
+    }
+
+    /**
+     * note that input's elements may be repeated ,and find the last position k where array[k] < v
+     * @param input
+     * @param v
+     */
+    void solutionLastPositionLess(int[] input,int v){
+        if (!checkLegal(input)){
+            System.out.println("input illegal .");
+            return ;
+        }
+
+        int position = -1;
+        int start = 0;
+        int end = input.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (input[mid] == v){
+                end = mid - 1;
+            }else if (input[mid] > v){
+                end = mid -1;
+            }else if(input[mid] < v){
+                start = mid +1;
+            }
+        }
+
+
+        //resulting state  end - start = 1,not available  position is -1,available
+        if (end >=0){
+            position = end;
+        }
+
+        System.out.println(String.format("the last position is array < %s is %s",v,position));
+    }
 
     public static void main(String[] args){
-        new BisectionSearchAndVariants().solutionBase(getInputData(),getTargetValue());
+        new BisectionSearchAndVariants().solutionLastPositionLess(getInputData(),getTargetValue());
     }
 
 }
