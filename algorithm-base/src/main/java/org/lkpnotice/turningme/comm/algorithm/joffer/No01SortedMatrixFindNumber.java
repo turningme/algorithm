c577082fd15f8f1c6b3882dd31c0c1ed55182e9c
liujinpeng04 <liujinpeng04@meituan.com>
2019-02-01 12:31:57 +0800

    bisearch variant ,search sorted two dimensons array

--- algorithm-base/src/main/java/org/lkpnotice/turningme/comm/algorithm/joffer/No01SortedMatrixFindNumber.java
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/comm/algorithm/joffer/No01SortedMatrixFindNumber.java
@@ -4,11 +4,76 @@ package org.lkpnotice.turningme.comm.algorithm.joffer;
  * Created by liujinpeng on 2019/1/24.
  *
      * Problem: given an matrix,which is n*n , row and column is sorted as asc,find int k, if k is in the matrix
+ * [1,2,3,4]
+ * [2,3,4,5]
+ * [3,4,5,6]
+ * [4,5,6,7]
+ * Solution: n*n 矩阵，行列都是升序，所以对角线也是升序，从 [0,0] 到 [n-1,n-1] 开始比较 如果小于[0,0]肯定没有，大于 [n-1,n-1] 也没有
+ * 可以结合二分查找，做两个版本，先做一个线性遍历查找，再做一个二分查找版本，利用有序的特点
+ * 突破n2，抵达线性的计算
+ * 二分法就是3logn = O(logn)
+ * 可以认为是与二分法殊途同归，都是利用一定的约束顺序，减枝，跳过不需要检测的空间
  *
- * Solution:
  *
  *
  */
 public class No01SortedMatrixFindNumber {
 
+    static int[][] getInputData(){
+        return new int[][]{
+                {1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}
+        };
+    }
+
+
+    String sulutionLinear(int[][] input,int size ,int key){
+        int midPosition = -1;
+        for(int n = 0; n <size; n++){
+            int tmp = input[n][n];
+            if (tmp == key){
+                return String.format("(%s,%s)",n,n);
+            }else if (key < tmp){
+                midPosition = n;
+            }
+        }
+
+        if (0==midPosition){
+            return String.format("not found too small");
+        }
+
+
+        if (-1 == midPosition){
+            return String.format("not found too big");
+        }
+
+
+        //find the diagonal position is between 1 and n-1
+
+
+        //the row
+        for(int i=0;i<midPosition;i++){
+            int rowEle = input[midPosition][i];
+            int colEle = input[i][midPosition];
+            if (rowEle == key){
+                return String.format("(%s,%s)",midPosition,i);
+            }
+            if (colEle == key){
+                return String.format("(%s,%s)",i,midPosition);
+            }
+        }
+
+
+        return String.format("Not found int the n*n matrix");
+    }
+
+
+
+
+
+
+
+
+    public static  void main(String[] args){
+        System.out.println(new No01SortedMatrixFindNumber().sulutionLinear(getInputData(),4,3));
+    }
 }
