e5dd44d9a3ebce877f7db8d407d4c05e7c3b38ee
liujinpeng04 <liujinpeng04@meituan.com>
2019-02-02 11:53:45 +0800

    frog jumping problem ,mathematics problem ,not using the force

---
+++ algorithm-base/src/main/java/org/lkpnotice/turningme/comm/algorithm/joffer/No10SmallRectanglesCoverBigOne.java
@@ -0,0 +1,15 @@
+package org.lkpnotice.turningme.comm.algorithm.joffer;
+
+/**
+ * Created by liujinpeng on 2019/2/2.
+ *
+ * Promblem: cover a rectangle of 2*n with small unit 1*2 without overlap, how many kinds of cover-up you can find ?
+ * Solution: Fibonacci
+ * 主要是自顶向下分析的过程中，确定递推公式，符合fibonacci 特点，主要还是要有分析过程
+ * 当然用递归解决这类分治法也可以，但是堆栈的深度有限
+ *
+ *
+ *
+ */
+public class No10SmallRectanglesCoverBigOne {
+}
